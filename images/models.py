27cc624c2c1d3e5137910202d021f578accdd370
Deep Sukhwani <deepsukhwani@gmail.com>
2020-11-26 12:32:34 +0530

    Add Images app and TemplateStore models
    - Add Images app, its associated settings and migrations
    - Enable Images app view from Django admin
    - Add TemplateStore model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class TemplateStore(models.Model):
+    """
+    Template store to contain template JSON in JSONField
+    """
+
+    name = models.CharField(max_length=30, null=False, blank=False)
+    template = models.JSONField()
+
+    def __str__(self) -> str:
+        """
+        String representation of model object
+        """
+        return str(self.name)

fd2b5bcb9c081803ecc33e26667c346b3b98096f
Deep Sukhwani <deepsukhwani@gmail.com>
2020-11-28 18:17:23 +0530

    Define data template model and add helper to generate data template
    - generate_data_templates_from_csv_data assists in generating data templates with appropriate values for `url` and `text`
    - Template model acts as object that stores templates with substituted values for `url` and `text`
    - Add logging for ease of future tracing/debugging
    
    - Add associated migrations due to model change

--- images/helpers.py
+++ images/helpers.py
@@ -1,3 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def update_url_and_text_in_template(template: dict, url: str, text: str) -> dict:
     """
     Given strings, `url` and `text`, set URL and text in the `template`
@@ -30,17 +35,51 @@ def update_url_and_text_in_template(template: dict, url: str, text: str) -> dict
         except TypeError:
             pass
 
+    logger.info(
+        f'Updated url to {url} and text to {text} in the following template: {template}'
+    )
     return template
 
 
-def generate_data_templates_from_csv_data(csv_file, template) -> None:
+def generate_data_templates_from_csv_data(csv_file) -> None:
     """
     Given CSV file, iterate over the data and generate final template that can be used as request body for IGS API
 
     The final template must be stored in `Template` Django model since storing them in memory can cause out of memory
     issues for large data sets.
+
+    Flow:
+        - Given the CSV file, iterate over all the rows (except first one, assuming first one is the headers)
+        - For each row of data, iterate over all the templates available in the TemplateStore table
+        - Substitute the `url` and `text` value from the data in the row in to the Template and store that template
+        - Mark that template as `processed=False`. Later when this will be actually processed (i.e. passed through
+        IGS API), we will mark it as `processed=True`
     """
-    pass
+    import csv
+
+    with open(csv_file, 'r') as csv_data:
+        reader = csv.reader(csv_data)
+        next(reader, None)    # skip the headers
+        for row in reader:
+            price, url = row[1], row[-1]
+
+            from .models import TemplateStore
+
+            # For all templates in TemplateStore
+            #
+            for template in TemplateStore.objects.all():
+                template_with_substituted_url_and_price = update_url_and_text_in_template(
+                    template, url=url, text=f'Now at ${price}')
+
+                from .models import Template
+                data_template_obj = Template.objects.create(
+                    csv_file_name=csv_file, processed=False, template=template_with_substituted_url_and_price
+                )
+                logger.info(
+                    f'Created data template: {data_template_obj}'
+                )
+
+        logger.info(f'Parsed CSV file: {csv_file} and created data template objects')
 
 
 def process_data_templates():
--- images/models.py
+++ images/models.py
@@ -14,3 +14,14 @@ class TemplateStore(models.Model):
         String representation of model object
         """
         return str(self.name)
+
+
+class Template(models.Model):
+    """
+    Template object to store templates created from templates in `TemplateStore`
+    These templates are created as part of helpers.generate_data_templates_from_csv_data
+    """
+
+    csv_file_name = models.CharField(max_length=30, null=False, blank=False)
+    processed = models.BooleanField(default=False)
+    template = models.JSONField()
